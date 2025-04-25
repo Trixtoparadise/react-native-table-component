d80b1b4063650696b27fe9aaf739f559805dd304
Trixtoparadise <102307608+Trixtoparadise@users.noreply.github.com>
2025-04-25 08:12:06 +0000

    test

--- components/rows.js
+++ components/rows.js
@@ -68,7 +68,7 @@ export class Rows extends Component {
                     renderItem={({item, index}) => {
                         const height = heightArr && heightArr[i];
                         return (
-                            <TouchableOpacity onPress={() => navigation(props.screen, {item: item, id: props.id[index]})}>
+                            <TouchableOpacity onPress={() => navigation(props.screen, {item: item, id: props.id[0]})}>
                                 <Row
                                     key={index}
                                     data={item}
